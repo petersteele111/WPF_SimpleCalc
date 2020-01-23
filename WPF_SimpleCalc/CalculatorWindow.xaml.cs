b1ed5bae68456e4369ba8bf52c47804a533cfd02
Peter Steele <steelep@mail.nmc.edu>
2020-01-23 15:44:35 -0500

    Initial Commit

--- WPF_SimpleCalc/CalculatorWindow.xaml.cs
+++ WPF_SimpleCalc/CalculatorWindow.xaml.cs
@@ -6,11 +6,11 @@ using System.Windows.Controls;
 namespace WPF_SimpleCalc
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CalculatorWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CalculatorWindow : Window
     {
-        public MainWindow()
+        public CalculatorWindow()
         {
             InitializeComponent();
         }
